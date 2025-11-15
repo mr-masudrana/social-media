03693026228f1d1c8d2ebf44213c17683748e27c
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2025-11-15 15:51:18 +0600

    Create Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+  return (
+    <div className="animate-pulse bg-white shadow rounded-xl p-4 mb-4">
+      <div className="h-3 bg-gray-300 rounded w-1/2"></div>
+      <div className="mt-3 h-40 bg-gray-300 rounded"></div>
+    </div>
+  );
+}
